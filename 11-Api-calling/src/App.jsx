f95138502d7a3135782361889d267b7cc50f92f6
Vikash Kumar <withnayak.dev@gmail.com>
2026-05-24 10:10:30 +0530

    api calling using axios

--- 11-Api-calling/src/App.jsx
+++ 11-Api-calling/src/App.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 const App = () => {
-    const [data, setdata] = useState([])
+     const [data, setdata] = useState([])
 
-  const getData = async () =>{
-    
-   const url =  await fetch('https://jsonplaceholder.typicode.com/albums')
-   const response = await url.json()
-   
-  setdata(response)
-  console.log('clicked',response)
-   }
-       return(
-         <div>
-            <button
-                onClick={getData}
-                >
-                Get Data
-            </button>
-            {data.map(function(elem,idx){
-                return <div key={idx}>
-                    <h3>hi {elem.id} and {elem.title}</h3>
-                </div>
-            })}
+    const getdata = async() =>{
+       
+        const url =await axios.get('https://picsum.photos/v2/list')
+
+        setdata(url.data)
+        console.log('click')
+         console.log(url)
+    }
+  return (
+    <div>
+        <button
+        onClick={getdata}>
+            Get Data
+        </button>
+        <div >
+           {data.map(function(elem,idx){
+            return <div key={idx}>
+                <h4>{idx} author name is {elem.author} </h4>
+            </div>
+           })}
         </div>
-    )
+    </div>
+   
+   
+  )
 }
 
 export default App
